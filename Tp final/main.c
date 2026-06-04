0e71113b58a9e594ad8dab4df8980a8d09cb399c
anavictoriaciardi-stack <anavictoriaciardi@gmail.com>
2026-06-04 16:53:26 -0300

    cambios

--- Tp final/main.c
+++ Tp final/main.c
@@ -8,7 +8,7 @@ int main()
 {
     printf("----------HOTEL 'LAS SOBREVIVIENTES'----------");
     printf("\n **|Bienvenido al sistema de gestion del hotel 'las sobrevivientes'. A continuacion podra elegir que acciones realizar.|**");
-    printf("\n1- Gestionar Huespedes \n2- Gestionar Habitaciones \n3- Gestionar Reservas \n0- salir del sistema de gestion")
+    printf("\n1- Gestionar Huespedes \n2- Gestionar Habitaciones \n3- Gestionar Reservas \n0- salir del sistema de gestion");
 
     return 0;
 }
